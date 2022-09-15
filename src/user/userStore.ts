0ae368a6aa23a8152248213f9a22637b6b7d14db
Sukrit Banthao <arm.xdsk@gmail.com>
2022-09-15 21:18:48 +0700

    📝 added code example to `useUserStore.update`

--- src/user/userStore.ts
+++ src/user/userStore.ts
@@ -3,6 +3,16 @@ import create from 'zustand'
 import { User } from '../core/sync-with-backend/dto/user/user'
 
 interface UserStore extends Partial<User> {
+  /**
+   * @example
+   * // 3 equivalent ways to update the store
+   * // 1
+   * update((prev) => ({ ...prev, name: 'new name' }))
+   * // 2
+   * update(() => ({ name: 'new name' }))
+   * // 3
+   * update({ name: 'new name' })
+   */
   update: (
     updater: ((prev: Partial<User>) => Partial<User>) | Partial<User>
   ) => void
