b5767d51ae93425f7ada80186c18235c1ca1de22
Sukrit Banthao <arm.xdsk@gmail.com>
2022-09-16 21:36:44 +0700

    🗃️ updated `userDtoSchema`

--- src/core/sync-with-backend/dto/user/user.ts
+++ src/core/sync-with-backend/dto/user/user.ts
@@ -1,10 +1,13 @@
 import { z } from 'zod'
 
+import { dateSchema } from '../../../constant/zodDateSchema'
+
 export const userDtoSchema = z.object({
   id: z.string().uuid(),
   email: z.string().email(),
   name: z.string(),
   surname: z.string(),
+  birthday: dateSchema,
 })
 
 export type User = z.infer<typeof userDtoSchema>
