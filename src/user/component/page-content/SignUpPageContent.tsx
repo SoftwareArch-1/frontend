dccf6e67a315a5c4fdbb175934102cd75ae5037d
Baimon664 <baimonperasit@gmail.com>
2022-09-17 10:30:27 +0700

    feat:add info signup ui

--- src/user/component/page-content/SignUpPageContent.tsx
+++ src/user/component/page-content/SignUpPageContent.tsx
@@ -1,8 +1,24 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form'
 import TextField from '../../../core/components/textField'
+import { pagePath } from '../../../core/utils/pagePath'
 import WithSignInBackground from '../withSigninBackground'
 
 const SignUpPageContent = () => {
+  const router = useRouter()
+
+  const { register, handleSubmit } = useForm<{
+    email: string
+    password: string
+    confirmPassword: string
+  }>()
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+    router.push(pagePath.SignUpInfoPage())
+  })
+
   return (
     <>
       <Head>
@@ -13,11 +29,15 @@ const SignUpPageContent = () => {
         <h1 className="mb-[90px] font-Inter text-4xl font-bold text-white">
           Create Your <span className="text-sky-500">Meety</span>
         </h1>
-        <form className="mb-[20px] flex flex-col items-center">
+        <form
+          className="mb-[20px] flex flex-col items-center"
+          onSubmit={onSubmit}
+        >
           <TextField
             label="Email Address"
             placeholder="enter your email address"
             id="email"
+            useFormRegisterReturn={register('email')}
           />
           <div className="h-[10px]"></div>
           <TextField
@@ -25,12 +45,14 @@ const SignUpPageContent = () => {
             placeholder="enter your password"
             id="password"
             hintText="require at least 8 characters"
+            useFormRegisterReturn={register('password')}
           />
           <div className="h-[10px]"></div>
           <TextField
             label="Confirm Password"
             placeholder="confirm your password"
-            id="comfirm-password"
+            id="confirmPassword"
+            useFormRegisterReturn={register('confirmPassword')}
           />
           <div className="h-[90px]"></div>
           <button
