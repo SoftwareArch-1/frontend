8b36d33677e3df7538b345aebacbff7f56e8a139
Sukrit Banthao <arm.xdsk@gmail.com>
2022-09-19 13:02:04 +0700

    ♻️ `emailSchema`

--- src/core/constant/zod/form-schema/signUpFormSchema.ts
+++ src/core/constant/zod/form-schema/signUpFormSchema.ts
@@ -1,11 +1,10 @@
 import { z } from 'zod'
+import { emailSchema } from '../emailSchema'
 
 import { passwordSchema } from '../passwordSchema'
 
 export const signUpFormSchema = z.object({
-  email: z.string().email({
-    message: 'Invalid email address',
-  }),
+  email: emailSchema,
   password: passwordSchema,
   confirmPassword: passwordSchema,
 })
