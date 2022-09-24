6c976c83fddb20bf1f06336a853fc254457be9f0
Sukrit Banthao <arm.xdsk@gmail.com>
2022-09-24 13:31:28 +0700

    🚸 wraps `<input>` with `<label>` when not specifying `htmlFor`

--- src/core/components/textField.tsx
+++ src/core/components/textField.tsx
@@ -23,33 +23,33 @@ const TextField: React.FC<{
     <div className="flex flex-col">
       <label className="flex flex-col text-sm text-white opacity-75">
         {props.label}
-      </label>
-      <div className="relative">
-        <input
-          className={classNames(
-            'h-[40px] w-[300px] border-b-2 border-sky-500 bg-transparent font-Inter text-base text-white placeholder-slate-100 focus:outline-none',
-            props.className
+        <div className="relative">
+          <input
+            className={classNames(
+              'h-[40px] w-[300px] border-b-2 border-sky-500 bg-transparent font-Inter text-base text-white placeholder-slate-100 focus:outline-none',
+              props.className
+            )}
+            id={props.id}
+            placeholder={props.placeholder}
+            type={inputType}
+            {...useFormRegisterReturn}
+          ></input>
+          {props.type === 'password' && (
+            <button
+              type="button"
+              className="absolute top-2 right-3"
+              onClick={() => {
+                setShowPassword((prev) => !prev)
+              }}
+            >
+              <IconifyIcon
+                icon={showPassword ? 'eyeOutline' : 'eyeOffOutline'}
+                className="text-white"
+              />
+            </button>
           )}
-          id={props.id}
-          placeholder={props.placeholder}
-          type={inputType}
-          {...useFormRegisterReturn}
-        ></input>
-        {props.type === 'password' && (
-          <button
-            type="button"
-            className="absolute top-2 right-3"
-            onClick={() => {
-              setShowPassword((prev) => !prev)
-            }}
-          >
-            <IconifyIcon
-              icon={showPassword ? 'eyeOutline' : 'eyeOffOutline'}
-              className="text-white"
-            />
-          </button>
-        )}
-      </div>
+        </div>
+      </label>
 
       {props.error && (
         <p className="font-Inter text-xs text-red-500">{props.error.message}</p>
