e4c370da028e7f4d8e7ac8bcd7b91f82ab9c84d0
Baimon664 <baimonperasit@gmail.com>
2022-09-24 09:33:49 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,8 @@ module.exports = {
   },
   plugins: [
     require('prettier-plugin-tailwindcss'),
-    require('@tailwindcss/forms'),
+    require('@tailwindcss/forms')({
+      strategy: 'class', // only generate classes
+    }),
   ],
 }
