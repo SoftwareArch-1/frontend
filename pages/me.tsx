cd0aae2bc9da816b5c49c725d64209902c11b615
Sukrit Banthao <arm.xdsk@gmail.com>
2022-09-25 21:36:51 +0700

    🚧 profile page route component `/me`

---
+++ pages/me.tsx
@@ -0,0 +1,8 @@
+import { ProfilePageContent } from '../src/user/component/page-content/ProfilePageContent'
+import { withAuth } from '../src/user/component/withAuth'
+
+const ProfilePage = () => {
+  return <ProfilePageContent />
+}
+
+export default withAuth(ProfilePage)
