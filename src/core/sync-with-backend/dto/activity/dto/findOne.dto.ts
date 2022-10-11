ef10b1bbf1d693c7772a8537c097c1dc415c1123
Sukrit Banthao <arm.xdsk@gmail.com>
2022-10-11 19:40:38 +0700

    👽️ update find one dto zod schema

--- src/core/sync-with-backend/dto/activity/dto/findOne.dto.ts
+++ src/core/sync-with-backend/dto/activity/dto/findOne.dto.ts
@@ -3,6 +3,27 @@ import { z } from 'zod'
 import { activityUser } from '../activity-user'
 import { ActivityModel } from '../zod'
 
+const base = ActivityModel.omit({
+  pendingUserIds: true,
+  joinedUserIds: true,
+}).extend({
+  ownerName: z.string(),
+  joinedUsers: activityUser.array(),
+  status: z.enum(['pending', 'joined', 'owned', 'not-joined']),
+})
+
+export const ActivityStatus = base.shape.status.enum
+
+export const findOneByOwner = base.extend({
+  pendingUsers: activityUser.array(),
+})
+
+export const findOneByNotOwner = base
+
+export type FindOneByOwner = z.infer<typeof findOneByOwner>
+
+export type FindOneByNotOwner = z.infer<typeof findOneByNotOwner>
+
 // const base = ActivityModel.omit({
 //   pendingUserIds: true,
 //   joinedUserIds: true,
