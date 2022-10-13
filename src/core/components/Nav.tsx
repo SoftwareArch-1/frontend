a54cc814e94350509ab85874c877eceae8c7b4b5
Baimon664 <baimonperasit@gmail.com>
2022-10-13 14:21:10 +0700

    edit review score

--- src/core/components/Nav.tsx
+++ src/core/components/Nav.tsx
@@ -1,7 +1,7 @@
 export const Nav = () => {
   return (
-    <nav className="flex bg-slate-700 p-[.625rem]">
+    <header className="flex bg-slate-700 p-[.625rem]">
       <span className="text-2xl font-bold text-sky-500">Meety</span>
-    </nav>
+    </header>
   )
 }
--- src/user/component/reviewSummary.tsx
+++ src/user/component/reviewSummary.tsx
@@ -10,20 +10,47 @@ export const ReviewSummary = (reviewsProps: {
   twoStar: number
   oneStar: number
 }) => {
-  const totalReviews = reviewsProps.fiveStar + reviewsProps.fourStar + reviewsProps.threeStar + reviewsProps.twoStar + reviewsProps.oneStar
-  const totalStars = reviewsProps.fiveStar*5 + reviewsProps.fourStar*4 + reviewsProps.threeStar*3 + reviewsProps.twoStar*2 + reviewsProps.oneStar
+  const totalReviews =
+    reviewsProps.fiveStar +
+    reviewsProps.fourStar +
+    reviewsProps.threeStar +
+    reviewsProps.twoStar +
+    reviewsProps.oneStar
+  const totalStars =
+    reviewsProps.fiveStar * 5 +
+    reviewsProps.fourStar * 4 +
+    reviewsProps.threeStar * 3 +
+    reviewsProps.twoStar * 2 +
+    reviewsProps.oneStar
   return (
     <section className="w-100 relative flex h-[127px] flex-row items-center justify-center rounded-lg bg-white p-6 drop-shadow-md">
-        <div className="pl-7 text-4xl">
-          <b>{totalReviews === 0? '0.0':(totalStars/totalReviews).toFixed(1)}</b>
-        </div>
-        <ul className="flex flex-col gap-2 pl-9 relative inset-y-1">
-          <FillUpBar label="5" value={reviewsProps.fiveStar ?? '50'} />
-          <FillUpBar label="4" value={reviewsProps.fourStar ?? '40'} />
-          <FillUpBar label="3" value={reviewsProps.threeStar ?? '30'} />
-          <FillUpBar label="2" value={reviewsProps.twoStar ?? '20'} />
-          <FillUpBar label="1" value={reviewsProps.oneStar ?? '5'} />
-        </ul>
+      <div className="pl-7 text-4xl">
+        <b>
+          {totalReviews === 0 ? '0.0' : (totalStars / totalReviews).toFixed(1)}
+        </b>
+      </div>
+      <ul className="relative inset-y-1 flex flex-col gap-2 pl-9">
+        <FillUpBar
+          label="5"
+          value={(reviewsProps.fiveStar / totalReviews) * 100}
+        />
+        <FillUpBar
+          label="4"
+          value={(reviewsProps.fourStar / totalReviews) * 100}
+        />
+        <FillUpBar
+          label="3"
+          value={(reviewsProps.threeStar / totalReviews) * 100}
+        />
+        <FillUpBar
+          label="2"
+          value={(reviewsProps.twoStar / totalReviews) * 100}
+        />
+        <FillUpBar
+          label="1"
+          value={(reviewsProps.oneStar / totalReviews) * 100}
+        />
+      </ul>
     </section>
   )
 }
